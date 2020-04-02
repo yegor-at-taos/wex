87374b884858d80121d7b8ac06d4c290dc348cf8
Yegor Gorshkov <ygorshkov@taos.com>
2020-04-01 19:32:41 -0700

    Satellite permission stack is now only applied globally.
    As IAM role is global there's no easy way to know where
    this Role is created and import in from CFN we just ask IAM
    if role is already in place and take no action if it is.

--- stack-satellite-iam.bash
+++ stack-satellite-iam.bash
@@ -2,19 +2,32 @@
 
 . shell-utils.bash
 
+### This is what's exported, not an internal Id
+role_name="WexRamCloudFormationCrossAccountRole"
+
+if [[ $(aws --profile "wex-$profile" --region "$region" \
+    iam list-roles | jq ".Roles
+        | map(select(.RoleName == \"$role_name\")) | length") -ne 0 ]]; then
+    echo "IAM Role already exists (this is not an error, you can go on)"
+    exit 0
+fi
+
+# We only need it in one region but let's keep the corporate naming scheme
 root="$account_name-cfn-satellite-permissions"
 stack="$root-$short_region-stk"
 
 json=$(remove_on_exit --suffix='.json')
 
-# Process 'json_source' -> 'json'
 json_source='json/satellite-permissions.json'
-json_address=$(jq '."Resources" | keys
-    | select("Wex.*Role")[0]' "$json_source")
+json_address=$(jq '.Resources | keys
+    | select("Wex.*Role")[0]' "$json_source" | sed -e 's/^"//;s/"$//')
 
 jq ".Resources.$json_address.Properties.Tags |= . +
-        $(jq .Mappings.Wex.Tags "$json_template")" \
-        "$json_source" > "$json"
+    $(jq .Mappings.Wex.Tags "$json_template")
+    | .Resources.$json_address.Properties.RoleName =
+        \"${role_name}\"
+    | .Outputs.${json_address}Export.Export.Name =
+        \"$role_name\"" "$json_source" > "$json"
 
 aws --profile "wex-$profile" --region "$region" \
     cloudformation "$(create_or_update "$stack")-stack" \
